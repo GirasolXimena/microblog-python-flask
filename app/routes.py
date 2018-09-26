7996456a9f78c04d1b4af5224e36355e197162e4
Robert Andrade <srandradejr@gmail.com>
2018-09-26 15:09:09 -0500

    added a way to find other users and started pagination of posts

--- app/routes.py
+++ app/routes.py
@@ -26,8 +26,9 @@ def index():
         db.session.commit()
         flash('Post successfully posted!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    return render_template('index.html', title='Home', form=form, posts=posts.items)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -126,3 +127,12 @@ def unfollow(username):
     db.session.commit()
     flash('You are no longer following {}!'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    return render_template("index.html", title='Explore', posts=posts.items)
