b8aedae731787d88a06b33ff48f94ed2fbdc1ac0
Robert Andrade <srandradejr@gmail.com>
2018-10-14 20:17:40 -0500

    formatting issuessues fixed and added moment.js support

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
 from datetime import datetime, timedelta
 import unittest
 from app import app, db
 from app.models import User, Post
+
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -13,10 +15,10 @@ class UserModelCase(unittest.TestCase):
         db.drop_all()
 
     def test_password_hashing(self):
-        u = User(username='sergio')
-        u.set_password('dog')
-        self.assertFalse(u.check_password('cat'))
-        self.assertTrue(u.check_password('dog'))
+        u = User(username='susan')
+        u.set_password('cat')
+        self.assertFalse(u.check_password('dog'))
+        self.assertTrue(u.check_password('cat'))
 
     def test_avatar(self):
         u = User(username='john', email='john@example.com')
@@ -25,8 +27,8 @@ class UserModelCase(unittest.TestCase):
                                          '?d=identicon&s=128'))
 
     def test_follow(self):
-        u1 = User(username='zavala', email='zavala@example.com')
-        u2 = User(username='ikora', email='ikora@example.com')
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
@@ -37,9 +39,9 @@ class UserModelCase(unittest.TestCase):
         db.session.commit()
         self.assertTrue(u1.is_following(u2))
         self.assertEqual(u1.followed.count(), 1)
-        self.assertEqual(u1.followed.first().username, 'ikora')
+        self.assertEqual(u1.followed.first().username, 'susan')
         self.assertEqual(u2.followers.count(), 1)
-        self.assertEqual(u2.followers.first().username, 'zavala')
+        self.assertEqual(u2.followers.first().username, 'john')
 
         u1.unfollow(u2)
         db.session.commit()
@@ -49,30 +51,30 @@ class UserModelCase(unittest.TestCase):
 
     def test_follow_posts(self):
         # create four users
-        u1 = User(username='zavala', email='zavala@example.com')
-        u2 = User(username='ikora', email='ikora@example.com')
-        u3 = User(username='cayde', email='cayde@example.com')
-        u4 = User(username='osiris', email='osiris@example.com')
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        u3 = User(username='mary', email='mary@example.com')
+        u4 = User(username='david', email='david@example.com')
         db.session.add_all([u1, u2, u3, u4])
 
         # create four posts
         now = datetime.utcnow()
-        p1 = Post(body="post from zavala", author=u1,
+        p1 = Post(body="post from john", author=u1,
                   timestamp=now + timedelta(seconds=1))
-        p2 = Post(body="post from ikora", author=u2,
+        p2 = Post(body="post from susan", author=u2,
                   timestamp=now + timedelta(seconds=4))
-        p3 = Post(body="post from cayde", author=u3,
+        p3 = Post(body="post from mary", author=u3,
                   timestamp=now + timedelta(seconds=3))
-        p4 = Post(body="post from osiris", author=u4,
+        p4 = Post(body="post from david", author=u4,
                   timestamp=now + timedelta(seconds=2))
         db.session.add_all([p1, p2, p3, p4])
         db.session.commit()
 
         # setup the followers
-        u1.follow(u2)  # zavala follows ikora
-        u1.follow(u4)  # zavala follows osiris
-        u2.follow(u3)  # ikora follows cayde
-        u3.follow(u4)  # cayde follows osiris
+        u1.follow(u2)  # john follows susan
+        u1.follow(u4)  # john follows david
+        u2.follow(u3)  # susan follows mary
+        u3.follow(u4)  # mary follows david
         db.session.commit()
 
         # check the followed posts of each user
@@ -84,6 +86,7 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(f2, [p2, p3])
         self.assertEqual(f3, [p3, p4])
         self.assertEqual(f4, [p4])
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
